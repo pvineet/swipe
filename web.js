6a3e7317ff236d26d7d227845841c456aa142993
pvineet <pvineet@gmail.com>
2015-11-18 19:15:34 +0530

    trying heroku

---
+++ web.js
@@ -0,0 +1,5 @@
+var gzippo = require('gzippo');
+var express = express();
+app.use(express.logger(''dev));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
