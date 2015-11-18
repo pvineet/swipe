a255dc929f2a776434dd277b83c540353b145b27
Vineet Panchbhaiyye <vineet@Vineets-MacBook-Pro.local>
2015-11-18 16:55:28 +0530

    Lets get started

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swipeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the swipeApp
+ */
+angular.module('swipeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
